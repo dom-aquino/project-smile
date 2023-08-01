03bbae70e7af8f9b83ac5099a8ba06ba83c95ca8
Dom Aquino <dom.aquino@outlook.com>
2023-08-01 19:55:21 +0200

    Add initial database configuration

--- main_app/forms.py
+++ main_app/forms.py
@@ -5,9 +5,9 @@ from wtforms.validators import DataRequired
 class AppointmentForm(FlaskForm):
     name = StringField('Customer Name', validators=[DataRequired()])
     appt_date = DateField('Appointment Date', validators=[DataRequired()],
-                          render_kw={"class": "appt_date"})
+        render_kw={"class": "appt_date"})
     appt_time = TimeField('Appointment Time', validators=[DataRequired()],
-                          render_kw={"class": "appt_time"})
+        render_kw={"class": "appt_time"})
     service = SelectField('Type of Service', choices=[
         ('consultation', 'Consultation'),
         ('teeth_whitening', 'Teeth Whitening'),
@@ -15,7 +15,6 @@ class AppointmentForm(FlaskForm):
         ('root_canal', 'Root Canal'),
         ('teeth_extraction', 'Teeth Extraction'),
         ('dental_filling', 'Dental Filling (Pasta)'),
-        ('oral-propylaxis', 'Oral Propylaxis (Cleaning)')
-    ])
+        ('oral-propylaxis', 'Oral Propylaxis (Cleaning)')])
     submit = SubmitField('Submit')
 
