168f4b737ef33eeb5291b5e6270447681e12ce6b
Dom Aquino <dom.aquino@outlook.com>
2023-08-05 23:03:02 +0200

    Add support for form submission

--- main_app/routes.py
+++ main_app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash
 from main_app import app
 from main_app.forms import AppointmentForm
 
@@ -7,9 +7,12 @@ from main_app.forms import AppointmentForm
 def index():
     return render_template("index.html", title="Welcome")
 
-@app.route("/appointment")
+@app.route("/appointment", methods=['GET', 'POST'])
 def appointment():
     form = AppointmentForm()
+    if form.validate_on_submit():
+        flash('Appointment has been set successfully. Date: {}, Time: {}'.format(
+            form.appt_date.data, form.appt_time.data))
     return render_template("appointment.html", title="Appointment", form=form)
 
 @app.route("/admin")
