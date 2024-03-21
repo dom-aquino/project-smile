3338777b3232ddcb288cb03a75f3290e9d6d18f6
Dom Aquino <dom.aquino@outlook.com>
2024-03-21 21:36:35 +0100

    Remove appointment.html

--- main_app/routes.py
+++ main_app/routes.py
@@ -3,10 +3,28 @@ from flask import render_template, flash
 from main_app import app
 from main_app.forms import AppointmentForm
 
+# Delete appointment.html file
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html", title="Welcome")
+    form = AppointmentForm()
+    if form.validate_on_submit():
+        appointment = Appointment(first_name=form.first_name.data,
+                                  last_name=form.last_name.data,
+                                  contact_number=form.contact_number.data,
+                                  service=form.service.data)
+        db.session.add(appointment)
+
+        schedule = Schedule(appt_date=form.appt_date.data,
+                            appt_time=form.appt_time.data,
+                            status=True, appointment=appointment)
+        db.session.add(schedule)
+        db.session.commit()
+
+        flash('Appointment has been set successfully. Date: {}, Time: {}'.format(
+            form.appt_date.data, form.appt_time.data))
+
+    return render_template("index.html", title="Welcome", form=form)
 
 @app.route("/appointment", methods=['GET', 'POST'])
 def appointment():
