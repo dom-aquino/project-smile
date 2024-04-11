a78f84acc7efa395d5097a28b72d83c943677e6c
Dom Aquino <dom.aquino@outlook.com>
2024-04-11 22:48:34 +0200

    Adjust navbar menu items

--- main_app/routes.py
+++ main_app/routes.py
@@ -26,6 +26,26 @@ def index():
     return render_template("index.html", title="Welcome", form=form)
 
 
+@app.route("/meet-the-doctors", methods=['GET'])
+def meet_the_doctors():
+    return render_template("meet_the_doctors.html", title="Meet the Doctors")
+
+
+@app.route("/faqs", methods=['GET'])
+def faqs():
+    return render_template("faqs.html", title="FAQs")
+
+
+@app.route("/locations", methods=['GET'])
+def locations():
+    return render_template("locations.html", title="Locations")
+
+
+@app.route("/contact-us", methods=['GET'])
+def contact_us():
+    return render_template("contact_us.html", title="Contact Us")
+
+
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
     appts = Appointment.query.all()
